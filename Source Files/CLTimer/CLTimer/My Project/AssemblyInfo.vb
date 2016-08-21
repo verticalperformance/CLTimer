b68b8ee5cfe514cbb5de2d9ff15738b8156dd86a
verticalperformance <keithbaddock@hotmail.com>
2016-08-21 10:59:24 +0000

    Moved variables from sets of 3 (one set per lane) to arrays Add saved settings Start work on dynamic COM port menu
    git-svn-id: http://192.168.0.101/svn/CLTimer/trunk@14 47567df9-5fb2-3842-a8b9-0d01033abebb

--- Source Files/CLTimer/CLTimer/My Project/AssemblyInfo.vb
+++ Source Files/CLTimer/CLTimer/My Project/AssemblyInfo.vb
@@ -12,7 +12,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("")> 
 <Assembly: AssemblyProduct("CLTimer")> 
-<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyCopyright("Copyright ©  2016")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
